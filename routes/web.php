8bb4dbaa7153549d72676d3d5cb17235f46e3265
Devdreamsolution <q3construction1@gmail.com>
2021-06-20 08:33:04 +0800

    Install auth

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use App\Http\Controllers\WelcomeController;
 */
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
