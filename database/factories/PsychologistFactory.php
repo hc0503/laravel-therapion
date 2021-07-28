45f1b3eca03fb830eeb27a378fe700cce2c505db
Devdreamsolution <q3construction1@gmail.com>
2021-07-28 18:02:40 +0900

    Add psychologist database

---
+++ database/factories/PsychologistFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Psychologist;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PsychologistFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Psychologist::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            
+        ];
+    }
+}
---
+++ database/migrations/2021_07_28_074231_create_psychologists_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePsychologistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('psychologists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('photo');
+            $table->string('email')->nullable();
+            $table->string('title')->nullable();
+            $table->text('info')->nullable();
+            $table->string('education')->nullable();
+            $table->text('topic')->nullable();
+            $table->text('about')->nullable();
+            $table->string('country_code')->default('en');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('psychologists');
+    }
+}
