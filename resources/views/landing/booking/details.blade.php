5fbab549f4b4a82cf12ef206e5a06949563a9962
Therapion <info@therapion.com>
2021-10-14 11:18:15 -0500

    Change content

--- resources/views/landing/booking/details.blade.php
+++ resources/views/landing/booking/details.blade.php
@@ -27,6 +27,7 @@
 						<div class="content-box">
 							<h1>{{ $psychologist->name }}</h1>
 							<span class="designation">{{ $psychologist->title }}</span>
+							<span class="designation">{{ $psychologist->education }}</span>
 							<div class="text">{{ $psychologist->about }}</div>
 						</div>
 					</div>
@@ -37,12 +38,11 @@
 	<div class="team-social-bar">
 		<div class="container clearfix">
 			<div class="content-box pull-right">
-				<div class="call"><i class="fas fa-phone"></i><a href="tel:(251)235-3256">(251) 235-3256</a></div>
+				<div class="call"><i class="fas fa-phone"></i><a href="tel:(+358) 9 2316 4346">(+358) 9 2316 4346</a></div>
 				<ul class="social-style-one">
-					<li><a href="mailto:{{ $psychologist->email }}"><i class="fab fa-google-plus-g"></i></a></li>
-					<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-					<li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-					<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+					<li><a href="https://www.facebook.com/therapion"><i class="fab fa-facebook-f"></i></a></li>
+					<li><a href="https://www.youtube.com/user/therapion"><i class="fab fa-youtube"></i></a></li>
+					<li><a href="https://www.linkedin.com/company/therapion-consulting"><i class="fab fa-linkedin"></i></a></li>
 				</ul>
 			</div>
 		</div>
@@ -53,43 +53,41 @@
 				<div class="col-lg-8 col-md-12 col-sm-12 content-side">
 					<div class="content-box">
 						<div class="content-style-one">
-							<h1>Biography</h1>
-							<div class="italic-text">I believe you have the power to make the changes you seek. There are times in our lives when we feel disempowered and disconnected from ourselves and others. I am here to walk with you on your journey towards healing.</div>
-							<div class="text">
-								<p>I will provide a safe, non-judgmental environment where you can work toward your own personal empowerment to overcome your struggles.I have experience working with those who struggle with Substance Abuse, Eating Disorders, Anxiety, Depression, and Trauma. My primary therapeutic modalities include. Cognitive Behavioral Therapy and Acceptance and Commitment Therapy.However, I work collaboratively with my clients to provide the most effective interventions possible based on their needs.</p>
-							</div>
-							<h3>Areas of Counselling Practice</h3>
-							<ul class="list">
-								<li>Adult children dealing with parents/caretakers who have personality disorders, and dementia</li>
-								<li>Affairs – working through the discovery and rebuilding trust</li>
-								<li>Self-esteem & confidence, assertive communication</li>
-								<li>Building better relationships (work and personal)</li>
-								<li>Complex mental health issues including dissociative disorders</li>
-								<li>Relationship issues including conflict & domestic violence</li>
-								<li>Anxiety including OCD, panic attacks, stress management</li>
-							</ul>
+							<h1>My Professional Profile</h1>
+
+							<div class="italic-text">Here are the key facts about my education and qualifications, 
+								areas of expertise, office hours plus a short biography.</div>
+						
+
+							<h3>My Titles</h3>
+
+							<div class="italic-text">{{ $psychologist->title }}</div>
+
+							<h3>Qualifications and Certificates</h3>
+
+							<div class="italic-text">{{ $psychologist->education }}</div>
+
+							<h3>Areas of Expertise</h3>
+
+							<div class="text">I can help you deal with the following issues: {{ $psychologist->topic }}</div>
+							
+							<h3>Office Hours</h3>
+
+							<div class="text">{{ $psychologist->info }}</div>	
+
 						</div>
+
 						<div class="content-style-two">
-							<h3>How I Approach Counselling and Psychotherapy</h3>
-							<div class="text">
-								<p>I support people in finding their strengths, building on them, and finding balance and contentment in the emotional, relational, physical, cognitive, and spiritual components of their life. I seek to create a safe and respectful environment for individuals and couples to nurture growth and healing.</p>
-								<p>I uphold the autonomy within those seeking help. I gently challenge them to courageously face life’s invariable disappointments outside the therapy room and to embrace opportunities to encourage others in their life journey.</p>
-							</div>
-							<h3>How I Approach Counselling and Psychotherapy</h3>
-							<ul class="list">
-								<li>PhD Psychology, University of Birmingham (UK)</li>
-								<li>MA Existential Phenomenological Psychology, Seattle University (USA)</li>
-								<li>BA Psychology, Western Washington University (USA)</li>
-								<li>Further training as a Trainee Clinical Psychologist (DClinPsy), University College of London (UK)</li>
-							</ul>
+							<h3>My Biography</h3>
+							<div class="text">{{ $psychologist->about }}</div>			 							
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-12 col-sm-12 sidebar-side">
 					<div class="sidebar gray-bg">
 						<div class="sidebar-form">
-						<h3>Free Consultation</h3>
-							<div class="text">Excepteur sint occaecat cupidatat pro ident sunt culpa officia desernt mollit</div>
+						<h3>Book A Session</h3>
+							<div class="text">Here you can book a personal counseling session with me.</div>
 							<form action="#" method="post">
 								<div class="form-group">
 									<label>Your Name</label>
@@ -100,18 +98,22 @@
 									<input type="email" name="name" required="">
 								</div>
 								<div class="form-group">
-									<label>Subject</label>
+									<label>Type of Session</label>
 									<div class="select-box">
 										<select class="selectmenu">
-											<option selected="selected">Defolt Sorting</option>
-											<option>Couples Therapy</option>
-											<option>Teens & Youth</option>
-											<option>Family Therapy</option>
+											<option selected="selected">Choose one</option>
+											<option>E-mail Counseling</option>
+											<option>Chat Counseling</option>
+											<option>Video Conferencing</option>
+											<option>Phone Counseling</option>
+											<option>Therapy Package # 1</option>
+											<option>Therapy Package # 2</option>
+											<option>Therapy Package # 3</option>
 										</select>
 									</div>
 								</div>
 								<div class="form-group">
-									<label>Info</label>
+									<label>Schedule Your Session</label>
 									<input type="email" name="info" required="">
 								</div>
 								<div class="form-group">
@@ -119,7 +121,7 @@
 									<textarea name="message"></textarea>
 								</div>
 								<div class="form-group message-btn">
-									<button type="submit" class="theme-btn">Send Message</button>
+									<button type="submit" class="theme-btn">Confirm Your Booking</button>
 								</div>
 							</form>
 						</div>
