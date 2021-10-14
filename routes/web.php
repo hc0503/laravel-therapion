eff1466737666bfb37843c3bc468964b153ee5a6
Devdreamsolution <q3construction1@gmail.com>
2021-10-14 23:26:22 +0900

    Set language param in route

--- routes/web.php
+++ routes/web.php
@@ -20,27 +20,32 @@ use App\Http\Controllers\DBMigrateController;
 |
 */
 
-Route::redirect('/', '/home');
-Route::get('/home', [LandingController::class, 'getHome'])->name('home');
-Route::group(['prefix' => 'services', 'as' => 'services.'], function() {
-	Route::get('/', [LandingController::class, 'getServices'])->name('all');
-	Route::get('/individuals', [LandingController::class, 'getIndividuals'])->name('individuals');
-	Route::get('/couples-and-families', [LandingController::class, 'getCouplesAndFamilies'])->name('couples-and-families');
-	Route::get('/communities', [LandingController::class, 'getCommunities'])->name('communities');
-	Route::get('/startups', [LandingController::class, 'getStartups'])->name('startups');
+Route::get('/', function() {
+	return redirect(app()->getLocale() . '/home');
 });
-Route::group(['prefix' => 'booking', 'as' => 'booking.'], function() {
-	Route::get('/', [LandingController::class, 'getBooking'])->name('all');
-	Route::get('/{psychologist}/details', [LandingController::class, 'getDetails'])->name('details');
+
+Route::group(['prefix' => '{locale}', 'middleware' => 'setlocale'], function() {
+	Route::get('/home', [LandingController::class, 'getHome'])->name('home');
+	Route::group(['prefix' => 'services', 'as' => 'services.'], function() {
+		Route::get('/', [LandingController::class, 'getServices'])->name('all');
+		Route::get('/individuals', [LandingController::class, 'getIndividuals'])->name('individuals');
+		Route::get('/couples-and-families', [LandingController::class, 'getCouplesAndFamilies'])->name('couples-and-families');
+		Route::get('/communities', [LandingController::class, 'getCommunities'])->name('communities');
+		Route::get('/startups', [LandingController::class, 'getStartups'])->name('startups');
+	});
+	Route::group(['prefix' => 'booking', 'as' => 'booking.'], function() {
+		Route::get('/', [LandingController::class, 'getBooking'])->name('all');
+		Route::get('/{psychologist}/details', [LandingController::class, 'getDetails'])->name('details');
+	});
+	Route::get('/fee-cost', [LandingController::class, 'getFeeCost'])->name('fee-cost');
+	Route::get('/jobs', [LandingController::class, 'getJobs'])->name('jobs');
+	Route::get('/training', [LandingController::class, 'getTraining'])->name('training');
+	Route::get('/contact-us', [LandingController::class, 'getContactUs'])->name('contact-us');
+	Route::get('/terms-of-service', [LandingController::class, 'getTermsOfService'])->name('terms-of-service');
+	Route::get('/privacy-policy', [LandingController::class, 'getPrivacyPolicy'])->name('privacy-policy');
+	Route::get('/about-us', [LandingController::class, 'getAboutUs'])->name('about-us');
+	Route::get('/friends', [LandingController::class, 'getFriends'])->name('friends');
 });
-Route::get('/fee-cost', [LandingController::class, 'getFeeCost'])->name('fee-cost');
-Route::get('/jobs', [LandingController::class, 'getJobs'])->name('jobs');
-Route::get('/training', [LandingController::class, 'getTraining'])->name('training');
-Route::get('/contact-us', [LandingController::class, 'getContactUs'])->name('contact-us');
-Route::get('/terms-of-service', [LandingController::class, 'getTermsOfService'])->name('terms-of-service');
-Route::get('/privacy-policy', [LandingController::class, 'getPrivacyPolicy'])->name('privacy-policy');
-Route::get('/about-us', [LandingController::class, 'getAboutUs'])->name('about-us');
-Route::get('/friends', [LandingController::class, 'getFriends'])->name('friends');
 
 Route::middleware(['auth'])->group( function () {
 	Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
@@ -49,8 +54,6 @@ Route::middleware(['auth'])->group( function () {
 		Route::resource('counselors', CounselorController::class);
 	});
 });
-
-Route::get('/lang/{locale}',[LanguageController::class, 'swap']);
 
 Auth::routes([
 	'register' => false
